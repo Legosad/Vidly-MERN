e258e1da3e317285439e2b197dcc4fb1e940ad40
Legosad <saadaltaf1202@gmail.com>
2024-10-17 11:18:16 +0500

    bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const { Users, validateUsers } = require("../models/users");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 //Get All
 
